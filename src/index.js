eb47f049ceb744cd611c952c540b9b33825b2740
Esau <dragonseturs@gmail.com>
2024-09-28 11:40:25 -0600

    Resolviendo mas problemas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const collection = require("./config");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 // convert data into json format
